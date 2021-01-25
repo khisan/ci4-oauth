d0f2922aff3a33f0ebcee4b7aaf75639dfa0ff68
khisan <khisan8@gmail.com>
2021-01-25 10:02:55 +0700

    add customoauth with user register

--- app/Libraries/Oauth.php
+++ app/Libraries/Oauth.php
@@ -2,7 +2,8 @@
 
 namespace App\Libraries;
 
-use \OAuth2\Storage\Pdo;
+// use \OAuth2\Storage\Pdo;
+use \App\Libraries\CustomOauthStorage;
 
 class Oauth
 {
@@ -19,7 +20,7 @@ class Oauth
     $username = getenv('database.default.username');
     $password = getenv('database.default.password');
 
-    $storage = new PDO(['dsn' => $dsn, 'username' => $username, 'password' => $password]);
+    $storage = new CustomOauthStorage(['dsn' => $dsn, 'username' => $username, 'password' => $password]);
     $this->server = new \OAuth2\Server($storage);
     $this->server->addGrantType(new \OAuth2\GrantType\UserCredentials($storage));
   }
